af14b578969498f61d94c935db03a236ec628c31
ABHISHEKDUBEY0074 <130496812+ABHISHEKDUBEY0074@users.noreply.github.com>
2025-08-22 15:52:35 +0530

    done

---
+++ veda-frontend/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center text-3xl font-bold text-red-500">
+      Tailwind Manual Setup Success!
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
