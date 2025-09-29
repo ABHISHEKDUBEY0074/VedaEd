891e75a4cb088e41fba9ecd225aaf989fb677d90
Shivam Anand <proff.shivam17anand@gmail.com>
2025-09-29 22:18:18 +0530

    added getClass by Id and by section and className

--- veda-backend/src/modules/class/classRoutes.js
+++ veda-backend/src/modules/class/classRoutes.js
@@ -4,7 +4,8 @@ const classController = require("./classController");
 
 // CRUD Routes for Classes
 router.get("/", classController.getClasses);        // GET all classes
-// router.get("/:id", classController.getClassById);   // GET class by id
+router.get("/:id", classController.getClassById ); // use it to display the list on Ui jaha class cards ke form me show ho rha, proper class teacher and sections isme mil jayega 
+router.get("/:classId/sections/:sectionId", classController.getClassByIdAndSection);   // GET class by id and section
 router.post("/", classController.createClass);      // POST new class
 router.put("/:id", classController.updateClass);    // PUT update class
 router.delete("/:id", classController.deleteClass); // DELETE class
