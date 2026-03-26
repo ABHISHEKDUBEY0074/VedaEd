8b84733567685d9bd86c8a46208091c1601e02ad
ABHISHEKDUBEY0074 <130496812+ABHISHEKDUBEY0074@users.noreply.github.com>
2026-03-26 12:31:08 +0530

    done

--- veda-frontend/src/AdmissionModule/DocumentVerification/DocumentVerification.jsx
+++ veda-frontend/src/AdmissionModule/DocumentVerification/DocumentVerification.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import config from "../../config";
 import {
